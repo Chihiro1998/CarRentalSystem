e3f285944ec929ccb844e7c6af4cf9f3bdf658ea
Chihiro1998 <935608397@qq.com>
2024-07-31 00:07:56 -0400

    Signup Form Create

--- car_rental_angular/src/app/app.component.spec.ts
+++ car_rental_angular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'car_rental_angular' title`, () => {
+  it(`should have as title 'Car_Rental_Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('car_rental_angular');
+    expect(app.title).toEqual('Car_Rental_Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, car_rental_angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Car_Rental_Angular');
   });
 });
--- car_rental_angular/src/app/auth/components/signup/signup.component.ts
+++ car_rental_angular/src/app/auth/components/signup/signup.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
-  standalone: true,
-  imports: [],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-
+  isSpinning: boolean = false;
 }
